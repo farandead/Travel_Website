e292bbd287369a163b087abd1af802ec8fd2fead
farandead <92890148+farandead@users.noreply.github.com>
2023-02-08 17:56:17 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,134 @@
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var subject = $('.validate-input input[name="subject"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(subject).val().trim() == ''){
+            showValidate(subject);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input1').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
+
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					fname: "required",
+					lname: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					fname: "Please enter your first name",
+					lname: "Please enter your last name",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/send-email.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeOut();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		}
+				
+			} );
+		}
+	};
+	contactForm();
+
+});
---
+++ js/switch-buttons.js
@@ -0,0 +1,206 @@
+let button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3")
+var button4 = document.getElementById("button4")
+var button5 = document.getElementById("button5")
+
+
+function button1change(){
+    var container1 = document.querySelector(".change-container-1");
+    var container2 = document.querySelector(".change-container-2");
+    var container3 = document.querySelector(".change-container-3");
+    var container4 = document.querySelector(".change-container-4");
+    var container5 = document.querySelector(".change-container-5");
+
+    let button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3")
+    var button4 = document.getElementById("button4")
+    var button5 = document.getElementById("button5")
+ 
+
+
+
+    button1.style.background = "rgb(7, 43, 94)";
+    button2.style.background = "white";
+    button3.style.background = "white";
+    button4.style.background = "white";
+    button5.style.background = "white";
+
+
+    button1.style.color = "white";
+    button2.style.color = "rgb(86, 85, 85)";
+    button3.style.color = "rgb(86, 85, 85)";
+    button4.style.color = "rgb(86, 85, 85)";
+    button5.style.color = "rgb(86, 85, 85)";
+
+    container1.style.display = "block";
+    container2.style.display = "none";
+    container3.style.display = "none";
+    container4.style.display = "none";
+    container5.style.display = "none";
+  
+    
+
+  
+
+    
+
+    
+}
+
+function button2change(){
+    var container1 = document.querySelector(".change-container-1");
+    var container2 = document.querySelector(".change-container-2");
+    var container3 = document.querySelector(".change-container-3");
+    var container4 = document.querySelector(".change-container-4");
+    var container5 = document.querySelector(".change-container-5");
+ 
+
+    container1.style.display = "none";
+    container2.style.display = "block";
+    container3.style.display = "none";
+    container4.style.display = "none";
+    container5.style.display = "none";
+
+    let button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3")
+    var button4 = document.getElementById("button4")
+    var button5 = document.getElementById("button5")
+ 
+
+
+
+    button1.style.background = "white";
+    button2.style.background = "rgb(7, 43, 94)";
+    button3.style.background = "white";
+    button4.style.background = "white";
+    button5.style.background = "white";
+
+
+    button1.style.color = "rgb(86, 85, 85)";
+    button2.style.color = "white";
+    button3.style.color = "rgb(86, 85, 85)";
+    button4.style.color = "rgb(86, 85, 85)";
+    button5.style.color = "rgb(86, 85, 85)";
+  
+
+}
+
+function button3change(){
+    var container1 = document.querySelector(".change-container-1");
+    var container2 = document.querySelector(".change-container-2");
+    var container3 = document.querySelector(".change-container-3");
+    var container4 = document.querySelector(".change-container-4");
+    var container5 = document.querySelector(".change-container-5");
+ 
+
+    container1.style.display = "none";
+    container2.style.display = "none";
+    container3.style.display = "block";
+    container4.style.display = "none";
+    container5.style.display = "none";
+
+    let button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3")
+    var button4 = document.getElementById("button4")
+    var button5 = document.getElementById("button5")
+ 
+
+
+
+    button1.style.background = "white";
+    button2.style.background = "white";
+    button3.style.background = "rgb(7, 43, 94)";
+    button4.style.background = "white";
+    button5.style.background = "white";
+
+
+    button1.style.color = "rgb(86, 85, 85)";
+    button2.style.color = "rgb(86, 85, 85)";
+    button3.style.color = "white";
+    button4.style.color = "rgb(86, 85, 85)";
+    button5.style.color = "rgb(86, 85, 85)";
+
+}
+
+function button4change(){
+    var container1 = document.querySelector(".change-container-1");
+    var container2 = document.querySelector(".change-container-2");
+    var container3 = document.querySelector(".change-container-3");
+    var container4 = document.querySelector(".change-container-4");
+    var container5 = document.querySelector(".change-container-5");
+
+    container1.style.display = "none";
+    container2.style.display = "none";
+    container3.style.display = "none";
+    container4.style.display = "block";
+    container5.style.display = "none";
+
+
+    let button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3")
+    var button4 = document.getElementById("button4")
+    var button5 = document.getElementById("button5")
+ 
+
+
+
+    button1.style.background = "white";
+    button2.style.background = "white";
+    button3.style.background = "white";
+    button4.style.background = "rgb(7, 43, 94)";
+    button5.style.background = "white";
+
+
+    button1.style.color = "rgb(86, 85, 85)";
+    button2.style.color = "rgb(86, 85, 85)";
+    button3.style.color = "rgb(86, 85, 85)";
+    button4.style.color = "white";
+    button5.style.color = "rgb(86, 85, 85)";
+    
+}
+
+
+
+
+function button5change(){
+    var container1 = document.querySelector(".change-container-1");
+    var container2 = document.querySelector(".change-container-2");
+    var container3 = document.querySelector(".change-container-3");
+    var container4 = document.querySelector(".change-container-4");
+    var container5 = document.querySelector(".change-container-5");
+
+    container1.style.display = "none";
+    container2.style.display = "none";
+    container3.style.display = "none";
+    container4.style.display = "none";
+    container5.style.display = "block";
+
+
+    let button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3")
+    var button4 = document.getElementById("button4")
+    var button5 = document.getElementById("button5")
+ 
+
+
+
+    button1.style.background = "white";
+    button2.style.background = "white";
+    button3.style.background = "white";
+    button4.style.background = "white";
+    button5.style.background = "rgb(7, 43, 94)";
+
+
+    button1.style.color = "rgb(86, 85, 85)";
+    button2.style.color = "rgb(86, 85, 85)";
+    button3.style.color = "rgb(86, 85, 85)";
+    button4.style.color = "rgb(86, 85, 85)";
+    button5.style.color = "white";
+    
+}
